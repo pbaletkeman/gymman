f8be4d4d321d7d6529776c507ccb5251ec3ee0bc
Pete Letkeman <pete@letkeman.ca>
2023-01-14 21:41:48 -0500

    second ascii doctor test added ascii doctor configuration updated

--- src/test/java/ca/letkeman/gymMan/asciiDoc/User.java
+++ src/test/java/ca/letkeman/gymMan/asciiDoc/User.java
@@ -1,11 +1,14 @@
-package ca.letkeman.gymMan;
+package ca.letkeman.gymMan.asciiDoc;
 
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.halLinks;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,7 +25,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-public class SampleJUnit5ApplicationTests {
+public class User {
 
   @Autowired
   private WebApplicationContext context;
@@ -38,14 +41,31 @@ public class SampleJUnit5ApplicationTests {
   }
 
   @Test
-  public void indexExample() throws Exception {
+  public void index() throws Exception {
     this.mockMvc.perform(get("/users")).andExpect(status().isOk())
-        .andDo(document("user",
+        .andDo(print())
+        .andDo(document("{ClassName}/{methodName}",
             links(
                 halLinks(), // <- this shortens things a bit
                 linkWithRel("self").ignored(),
+//                linkWithRel("links").ignored(),
+                //linkWithRel("profile").description("The ALPS profile for the service"))
                 linkWithRel("profile").ignored()
+            )
+        ));
+
+  }
+  @Test
+  public void single() throws Exception {
+    this.mockMvc.perform(get("/users/1")).andExpect(status().isOk())
+        .andDo(print())
+        .andDo(document("{ClassName}/{methodName}",
+            links(
+                halLinks(), // <- this shortens things a bit
+                linkWithRel("self").ignored(),
+                linkWithRel("creator").ignored(),
+                //linkWithRel("_links").ignored(),
+                linkWithRel("user").ignored()
             )));
   }
-
 }
